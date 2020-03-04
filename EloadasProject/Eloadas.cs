35ec8eda42298892bb34ca00d7aefeeef98f0ecd
Szanto Monika <szantomomka@gmail.com>
2020-03-04 15:10:29 +0100

    2. feladat a, b,

--- EloadasProject/Eloadas.cs
+++ EloadasProject/Eloadas.cs
@@ -4,5 +4,15 @@ namespace EloadasProject
 {
     public class Eloadas
     {
+        private bool[,] foglalasok;
+        public Eloadas(int sorokSzama, int helyekSzama)
+        {
+            if (!(sorokSzama > 0) || !(helyekSzama > 0))
+            {
+                throw new ArgumentException("A paramétereknek 0-nál nagyobbnak kell lenniük.");
+            }
+            foglalasok = new bool[sorokSzama, helyekSzama];
+        }
+
+
     }
-}
